de705d62ed16c10de8b455f38811f4bcd0f56ab4
Thales Kenne <thales.kenne@altimus.com.br>
2021-04-26 20:48:18 -0300

    Adding scroll

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,21 +1,54 @@
-import styles from './carousel.module.css';
+import { useState } from 'react';
+
+// Libraries
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+// Styles
+import styles from './carousel.module.css';
+
 const Carousel = () => {
+  const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleBackClick = () => {
+    const nextIndex = currentIndex - 4;
+    if (nextIndex <= 0) {
+      /*only goes back the difference between the negative result and 
+      the usual 4 steps*/
+      const remainingItems = nextIndex + 4;
+      setCurrentIndex(currentIndex - remainingItems);
+    } else {
+      setCurrentIndex(nextIndex);
+    }
+  }
+
+  const handleNextClick = () => {
+    const nextIndex = currentIndex + 4;
+    if ((nextIndex + 4) > items.length) {
+      const remainingItems = items.length - nextIndex;
+      setCurrentIndex(currentIndex + remainingItems)
+    } else {
+      setCurrentIndex(nextIndex)
+    }
+  }
+
+
   return (
     <div className={styles.carouselContainer}>
-      <FiChevronLeft className={styles.arrow} />
+      <FiChevronLeft className={styles.arrow} onClick={handleBackClick} />
+
       <div className={styles.blocksContainer}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => (
-          <div className={styles.block}>
+        {items.map((item, itemIndex) => (
+          <div className={styles.block} style={{ left: 250 * (itemIndex - currentIndex) }}
+          >
             <h1>{item}</h1>
           </div>
         ))}
 
       </div>
 
-      <FiChevronRight className={styles.arrow} />
-    </div>
+      <FiChevronRight className={styles.arrow} onClick={handleNextClick} />
+    </div >
   )
 }
 
