6f9b780300f7a470d2ef420f30b4fc46efa88e85
Thales Kenne <thales.kenne@altimus.com.br>
2021-04-26 22:56:03 -0300

    Adding logo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 
 import Carousel from './components/carousel';
 
-
+// Assets
+import Logo from './assets/logo.png';
 import styles from './app.module.css';
 
 function App() {
   return (
     <div className={styles.container}>
+      <img src={Logo} alt="logo" height={350} />
       <Carousel />
+      <span className={styles.credits}>Developed by <a href="https://github.com/sirkadogan" target="_blank" rel="noreferrer">Thales Kenne</a></span>
     </div>
   );
 }
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,17 +1,24 @@
 
+import { useMemo } from 'react';
 import styles from './card.module.css';
 
 const Card = ({ item, style }) => {
   const { title, images } = item;
+
+  const pickImage = () => {
+    return images[Math.floor(Math.random() * images.length)];
+  }
+
+  const selectedImage = useMemo(() => pickImage(), [title]);
 
   return (
 
     <div
       className={styles.card}
       style={style}>
-      <a href={images[0]} target="_blank" rel="noreferrer">
+      <a href={selectedImage} target="_blank" rel="noreferrer">
         <div className={styles.cardContent}>
-          <img src={images[0]} alt="Test" />
+          <img src={selectedImage} alt="Test" />
           <div className={styles.cardBody}>
             <h2>{title}</h2>
             <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In illo adipisci reprehenderit eum omnis maxime, corrupti enim aspernatur doloremque quas hic </span>
