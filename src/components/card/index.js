15a2395f22981928b02d1369f2628ea572143092
Thales Kenne <thales.kenne@altimus.com.br>
2021-04-26 22:16:49 -0300

    Link to image page

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -5,19 +5,21 @@ const Card = ({ item, style }) => {
   const { title, images } = item;
 
   return (
+
     <div
       className={styles.card}
       style={style}>
-      <div className={styles.cardContent}>
-
-        <img src={images[0]} alt="Test" />
-        <div className={styles.cardBody}>
-
-          <h2>{title}</h2>
-          <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In illo adipisci reprehenderit eum omnis maxime, corrupti enim aspernatur doloremque quas hic </span>
+      <a href={images[0]} target="_blank" rel="noreferrer">
+        <div className={styles.cardContent}>
+          <img src={images[0]} alt="Test" />
+          <div className={styles.cardBody}>
+            <h2>{title}</h2>
+            <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In illo adipisci reprehenderit eum omnis maxime, corrupti enim aspernatur doloremque quas hic </span>
+          </div>
         </div>
-      </div>
-    </div>)
+      </a>
+    </div>
+  )
 
 }
 
